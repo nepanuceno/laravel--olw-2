65daa359244fd9e1ef2d07bb860aef8171d3fdec
Paulo Roberto Torres <paulotorres@mpto.mp.br>
2023-10-25 18:00:21 -0300

    Create migrations of tables, index and view

--- database/migrations/2023_10_25_201359_create_user_email_index.php
+++ database/migrations/2023_10_25_201359_create_user_email_index.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,13 +12,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('clients', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->constrained();
-            $table->foreignId('address_id')->constrained();
-            $table->timestamps();
-            $table->softDeletes();
-        });
+        DB::unprepared("CREATE UNIQUE INDEX user_email_index on users(email) WHERE deleted_at IS NULL");
     }
 
     /**
@@ -25,6 +20,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('clients');
+        Schema::table('users', function(Blueprint $table){
+            $table->dropIndex('user_email_index');
+        });
     }
 };
