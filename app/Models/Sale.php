e7edf562197642620c62871fa6f7c549b41ce710
Paulo Roberto Torres <paulotorres@mpto.mp.br>
2023-10-25 18:25:34 -0300

    Create models

---
+++ app/Models/Sale.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Sale extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['seller_id', 'client_id', 'sold_at', 'status', 'total_amount'];
+
+    public function client(): BelongsTo
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function seller(): BelongsTo
+    {
+        return $this->belongsTo(Seller::class);
+    }
+}
---
+++ app/Models/Seller.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Seller extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'company_id'
+    ];
+
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
